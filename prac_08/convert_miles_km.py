339332530920701ffd50fce01bec24bbc2c9c849
jmoci <jessemocilac@gmail.com>
2024-11-19 21:43:41 +1000

    Extend functionality to handle entering miles input as well as converting to km and displaying output

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,14 +1,27 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.properties import StringProperty
+
 
 class MilesToKMApp(App):
     """ MilesToKMApp is a Kivy App for converting miles to kilometers """
+
+    message = StringProperty()
+
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (400, 200)
+        Window.size = (600, 200)
         self.title = "Convert Mi to Km"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.message = "Convert miles to km"
         return self.root
+
+    def handle_convert_mi_to_km(self, miles):
+        try:
+            result = float(miles)*1.60934
+            self.message = str(result)
+        except ValueError:
+            pass
 
 MilesToKMApp().run()
