634b15002c1ba76a7ffdd36246f42ded48287d4b
jmoci <jessemocilac@gmail.com>
2024-11-24 21:14:35 +1000

    Override SilverServiceTaxi get_fare method and create test

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -9,3 +9,7 @@ class SilverServiceTaxi(Taxi):
     def __init__(self, name, fuel,fanciness:float):
         super().__init__(name, fuel)
         self.price_per_km *= fanciness
+
+    def get_fare(self):
+        # Get the cost of the current fare
+        return self.flagfall + self.price_per_km * self.current_fare_distance
--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -6,3 +6,11 @@ print("Test constructor")
 s1 = SilverServiceTaxi("Limo 1",100,2.5)
 assert s1.price_per_km == 2.5*Taxi.price_per_km
 print(s1)
+
+#Test get fare
+print("Test get fare")
+s2 = SilverServiceTaxi("Limo 2",100,2.5)
+print(s2.get_fare())
+s2.drive(40)
+print(s2.get_fare())
+assert s2.get_fare() == 40*Taxi.price_per_km*2.5 + SilverServiceTaxi.flagfall
