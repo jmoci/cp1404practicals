5091a2cec47ab7e494364ad3d3371dedd077014c
jmoci <jessemocilac@gmail.com>
2024-11-24 21:43:43 +1000

    Create taxi_simulator display_main_menu function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,7 +1,24 @@
 """This module contains a taxi simulator program"""
 
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MAIN_MENU = "q)uit, c)hoose taxi, d)rive"
+
 def main():
-    pass
+    print("Let's drive!")
+    display_main_menu()
+
+def display_main_menu():
+    # Present the user with a list of menu options
+    print(MAIN_MENU)
+    choice = input(">>>").upper()
+    while choice != 'Q':
+        if choice == 'C':
+            pass
+        if choice == 'D':
+            pass
+        choice = input(">>>").upper()
 
 if __name__ == '__main__':
     main()
