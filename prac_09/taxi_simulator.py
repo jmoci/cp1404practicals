f4217b69d07cdb49effe1db56bf0e9db0fe7ddc4
jmoci <jessemocilac@gmail.com>
2024-11-24 21:59:16 +1000

    Create handle_drive_taxi function in taxi_simulator module

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -11,14 +11,37 @@ def main():
 
 def display_main_menu():
     # Present the user with a list of menu options
+    #chosen_taxi = None
+    bill = 0
+    chosen_taxi = Taxi("Toyota 1",100)
     print(MAIN_MENU)
     choice = input(">>>").upper()
     while choice != 'Q':
         if choice == 'C':
             pass
         if choice == 'D':
-            pass
+            bill += handle_drive_taxi(chosen_taxi)
+            print(f"Bill to date: ${bill:.2f}")
+        print(MAIN_MENU)
         choice = input(">>>").upper()
 
+def handle_drive_taxi(taxi):
+    if taxi is None:
+        print("You need to choose a taxi before you can drive")
+        return 0
+    input_valid = False
+    while not input_valid:
+        try:
+            distance = float(input("Drive how far?"))
+            if distance < 0:
+                print("Distance must be a positive number")
+            else:
+                input_valid = True
+                taxi.start_fare()
+                taxi.drive(distance)
+                print(f"Your {taxi.name} trip cost you {taxi.get_fare()}")
+                return taxi.get_fare()
+        except ValueError:
+            print("Distance must be a valid number")
 if __name__ == '__main__':
     main()
