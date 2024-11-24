339fe3720eff9ea1ec748c66c734d029e477ccc5
jmoci <jessemocilac@gmail.com>
2024-11-24 21:02:04 +1000

    Create drive method for UnreliableCar class and test

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -9,3 +9,8 @@ class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        if randint(0, 100) < self.reliability:
+            return super().drive(distance)
+        return 0
--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -4,3 +4,11 @@ from prac_09.unreliable_car import UnreliableCar
 print("Test constructor")
 u1 = UnreliableCar("Jeep 1",20,50)
 print(u1)
+
+#Test drive method
+print("Test drive method")
+results = []
+for i in range(0,10):
+    results.append(u1.drive(1))
+print(results)
+print(u1)
