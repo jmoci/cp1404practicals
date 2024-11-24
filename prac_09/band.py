aa0c4f2255d4bfbf34f400a5b5a5d888e672998e
jmoci <jessemocilac@gmail.com>
2024-11-24 22:29:37 +1000

    Remove import and static Musician typing from band module

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,6 +1,4 @@
 """This module contains the definition for the band class"""
-from musician import Musician
-
 
 class Band:
     def __init__(self,name):
@@ -10,5 +8,5 @@ class Band:
     def __str__(self):
         return str([musician for musician in self.musicians])
 
-    def add(self,musician:Musician):
+    def add(self,musician):
         self.musicians.append(musician)
