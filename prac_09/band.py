a3b2058b63d432eaad32ec1ba91c157314dc155e
jmoci <jessemocilac@gmail.com>
2024-11-24 22:25:54 +1000

    Create Band "add" method

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,6 +1,11 @@
 """This module contains the definition for the band class"""
+from musician import Musician
 
 
 class Band:
     def __init__(self,name):
         self.name = name
+        self.musicians = []
+
+    def add(self,musician:Musician):
+        self.musicians.append(musician)
