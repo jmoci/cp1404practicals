c6cc359233c4e914a51054948e192cc9e1e9b001
jmoci <jessemocilac@gmail.com>
2024-11-24 21:33:55 +1000

    Modify drive method to use calculation from Taxi superclass

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -15,4 +15,4 @@ class SilverServiceTaxi(Taxi):
 
     def get_fare(self):
         # Get the cost of the current fare
-        return self.flagfall + self.price_per_km * self.current_fare_distance
+        return self.flagfall + super().get_fare()
