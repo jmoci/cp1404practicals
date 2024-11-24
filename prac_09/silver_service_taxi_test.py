e19a1947fc24d7d0b665cec903d57272121cf67e
jmoci <jessemocilac@gmail.com>
2024-11-24 21:23:51 +1000

    Update SilverServiceTaxi test to use constants

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -13,7 +13,7 @@ s2 = SilverServiceTaxi("Limo 2",100,2.5)
 print(s2.get_fare())
 s2.drive(40)
 print(s2.get_fare())
-assert s2.get_fare() == 40*Taxi.price_per_km*2.5 + SilverServiceTaxi.flagfall
+assert s2.get_fare() == 40*1.23*2.5 + 4.5
 
 #Test __str__
 print("Test __str__")
