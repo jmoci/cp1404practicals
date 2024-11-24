4fce435ecc69f5d40b12e6e4f0634a7a4859007e
jmoci <jessemocilac@gmail.com>
2024-11-24 21:35:10 +1000

    Modify taxi test to reflect changes to Taxi class

--- prac_09/test_taxi.py
+++ prac_09/test_taxi.py
@@ -3,12 +3,12 @@
 from taxi import Taxi
 
 def main():
-    my_taxi:Taxi = Taxi("Prius 1",100)
+    my_taxi:Taxi = Taxi("Prius 1",400)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
-    my_taxi.drive(100)
+    my_taxi.drive(101)
     print(my_taxi)
-
+    print(my_taxi.get_fare())
 if __name__ == "__main__":
     main()
