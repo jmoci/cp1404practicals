4c933b5c08bf0708a473704f0e72cef89ad700a3
jmoci <jessemocilac@gmail.com>
2024-12-01 23:28:03 +1000

    Update wiki module to handle disambiguation exception

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,12 +1,18 @@
+"""This module includes a program to get a wikipedia summary based on a user prompt."""
+
+
 import wikipedia
-from wikipedia import suggest
 
 
 def main():
     prompt = input("Enter page title: ")
     while prompt != "":
         result = wikipedia.search(prompt)[0]
-        print(wikipedia.summary(result,auto_suggest = False))
+        try:
+            print(wikipedia.summary(result,auto_suggest = False))
+        except wikipedia.exceptions.DisambiguationError as e:
+            print("we need a more specific title. Try one of the following, or a new search:")
+            print(e.options)
         prompt = input("Enter page title: ")
 
 if __name__ == '__main__':
